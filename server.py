6df63aeb4ad48c6259b6875bb2b655b0670b15f3
thirumurug0xan <thirumurugann28@gmail.com>
2024-11-29 08:50:40 +0000

    new file:   about_us.html 	new file:   contact_us.html 	new file:   coreserver.py 	new file:   home.html 	new file:   home2.html 	new file:   home2.jpeg 	new file:   index.html 	new file:   login.html 	new file:   register.html 	new file:   server.py 	new file:   view_product.html

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__,template_folder='./')
+
+# Route: Homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route: Products Page
+# @app.route('/products')
+# def products():
+#     # Connect to the database
+#     conn = sqlite3.connect('database.db')
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT name, price FROM products")
+#     products = cursor.fetchall()
+#     conn.close()
+
+#     # Pass product data to the template
+#     return render_template('products.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
